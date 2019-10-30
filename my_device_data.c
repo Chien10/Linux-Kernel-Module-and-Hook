e454ddab93c8111065eacfa74265bc5ca9336d19
Chiến Minh <17082958+Chien10@users.noreply.github.com>
2019-10-30 12:05:52 +0700

    Adding open, read, write and ioctl functionalities

--- my_device_data.c
+++ my_device_data.c
@@ -1,8 +1,11 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
+#include <asm/unaccess.h>
+#include <asm/ioctl.h>
 
 #define MY_MAJOR 42
 #define MY_MAX_MINORS 5
+#define MY_IOCTL_IN _IOC(_IOC_WRITE, 'k', 1, sizeof my_ioctl_data)
 
 // dev_t keeps the identifiers of a device file and can be obtained using MKDEV macro
 // The following functions are for the static assignment and unallocation of device identifiers
@@ -72,11 +75,68 @@ static int my_open(struct inode *inode, struct file *file)
 	my_data = container_of(ignore->i_cdev, struct my_device_data, cdev);
 
 	file -> private_data = my_data;
+
+	return 0;
 }
 
 static int my_read(struct file *file, char __user *user_buffer, size_t size, loff_t *offset)
 {
-	struct my_device_data *my_data;
+	struct my_device_data *my_data = (struct my_device_data*) file -> private_data;
+	
+	ssize_t len = min(my_data -> size - *offset, size);
+	if (len <= 0) {
+		return 0;
+	}
 
-	my_data = (struct my_device_data *) file -> private_data;
+	if (copy_to_user(user_buffer, my_data -> buffer + *offset, len)) {
+		return -EFAULT;
+	}
+
+	*offset += len;
+
+	return len;
 }
+
+static int my_write(struct file *file, const char __user *user_buffer, ssize_t size, loff_t *offset)
+{
+	struct my_device_data *my_dat = (struct my_device_data*) file -> private_data;
+
+	size_t len = min(my_data -> size - *offset, size);
+	if (len <= 0) {
+		return 0;
+	}
+
+	if (copy_from_user(my_data -> buffer + *offset, user_buffer, len)) {
+		return -EFAULT;
+	}
+
+	*offset += len;
+
+	return len;
+
+}
+
+static long my_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+	struct my_device_data *my_data = (struct my_device_data*) file -> private_data;
+	my_ioctl_data mid;
+
+	if (cmd == MY_IOCTL_IN)
+	{
+		if (copy_from_user(&mid, (my_ioctl_data *) arg, sizeof my_ioctl_data)) {
+			return -EFAULT;
+		}
+
+		/* Process data and execute command */
+
+		break;
+	}
+	else {
+		return -ENOTTY;
+	}
+
+	return 0;
+}
+
+// Access to address user-space driver device of a process
+// Direct access of user-space can lead to incorrect behaviour and security issues.
